6c726fa233e4bb3684cb8279a69ebf8c7fa69cd1
Ali Moghnieh <SupaHam.me@Gmail.com>
2014-02-10 00:38:06 +0000

    Initial commit.

---
+++ src/main/java/com/supaham/supachatapi/SupaChatAPI.java
@@ -0,0 +1,16 @@
+package com.supaham.supachatapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Main class for the SupaChatAPI plugin. This is not required for the API to function.
+ * This class extends {@link org.bukkit.plugin.java.JavaPlugin} for convenience.
+ */
+public class SupaChatAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Nothing to do here.
+    }
+
+}
